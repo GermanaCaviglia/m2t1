8d7afd5e08d7a3139793a16df81b89b35b4c2952
Germana <germiicaviglia@gmail.com>
2022-10-14 20:35:21 -0300

    :tada: First Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const   firstName = "Germana", // string
+        lastName = "Caviglia", // string
+        favColor = "Azul"; // string
+
+var     myAge = 24 , // number
+        hasNephews = true, // boolean
+        hasBrothers = true , // boolean
+        brothers = { // object
+            numberOfBrothers : 2, // number
+            brother1 : { // object
+                name1 : "Mauricio", // string
+                age1 : 39,
+                profession1 : "Panadero" // string 
+            },
+            brother2 : { // object
+                name2 : "Leopoldo", // string
+                age2 : 37,    
+                profession2 : "Tecnico en Sonido", // string
+                nameOfHisSon : "Filippo" // string
+            }    
+        };
+        hasSisters = false ; // boolean
+        favSongs = ["Seven Days in Sunny June", "Keep Moving", "Aguas de Março", "Fragments of Tine"]; // array
+        pets = { // object
+            nina : { // object
+                isADog : true, // boolean 
+                is7YearsOld : false, // boolean
+                petsAge : 10, // number 
+                characteristics : [sweet, calm, active, spontaneos] // array
+            },
+            annika : { // object
+                isACat : false, // boolean
+                is4YearsOld: true, // boolean
+                petsAge : 4, // number 
+                characteristics : [noisy, energetic, intelligent, charming] // array
+            }
+        }
